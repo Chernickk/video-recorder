fc6ee5245306397daf7c57caac13b409e66d310f
nik <17rolex@gmail.com>
2021-11-19 21:43:14 +0300

    fix

--- utils/redis_client.py
+++ utils/redis_client.py
@@ -1,13 +1,13 @@
 import redis
 
 redis_client = redis.Redis(
-    host='localhost',
+    host='127.0.0.1',
     db=0,
     decode_responses=True
 )
 
 redis_client_pickle = redis.Redis(
-    host='localhost',
+    host='127.0.0.1',
     db=0,
     decode_responses=False
 )
